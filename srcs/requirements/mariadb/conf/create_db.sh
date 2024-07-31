2b6acae700ac274962d41210d7f01f8f936060fc
yassine <you@example.com>
2024-07-31 13:38:41 -0500

    add mariadb

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -32,3 +32,16 @@
 #         /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
 #         rm -f /tmp/create_db.sql
 # fi
+
+#!/bin/bash
+
+service mariadb start
+
+mysql -u root -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+mysql -u root -e "GRANT ALL ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+service mariadb stop
+
+exec mysqld -u mysql
